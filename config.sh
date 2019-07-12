dd2792bb28081302e4dd13861d542857d497d8c9
Hsiaoming Yang <me@lepture.com>
2019-07-12 21:27:49 +0900

    Fix config.sh

--- config.sh
+++ config.sh
@@ -2,16 +2,16 @@
 set -e
 
 if [ ! -f /data/server.properties ]; then
-    cp /bedrock-server/server.properties /data/
+    mv /bedrock-server/server.properties /data/
     ln -s /data/server.properties /bedrock-server/server.properties
 fi
 
 if [ ! -f /data/permissions.json ]; then
-    cp /bedrock-server/permissions.json /data/
+    mv /bedrock-server/permissions.json /data/
     ln -s /data/permissions.json /bedrock-server/permissions.json
 fi
 
 if [ ! -f /data/whitelist.json ]; then
-    cp /bedrock-server/whitelist.json /data/
+    mv /bedrock-server/whitelist.json /data/
     ln -s /data/whitelist.json /bedrock-server/whitelist.json
 fi
